bf2520196df5237ba8f14d416df91d9a42f69b3c
DanaCriolloLopez <91813240+DanaCriolloLopez@users.noreply.github.com>
2023-05-27 23:55:24 -0500

    1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Inicio;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Inicio::class, 'inicio']);
